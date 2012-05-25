f18f9a19b2bc66bf662bc72f38eaa45da7cca29e
bastien.gandouet@student.ecp.fr <bastien.gandouet@student.ecp.fr>
2012-05-25 16:56:20 +0200

    Working graphgen.py with some examples on socialgraph.py

---
+++ socialgraph.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+
+import networkx as nx
+import graphgen
+import matplotlib.pyplot as plt
+
+
+#Generating a Graph with 5 nodes
+G = graphgen.generator(5)
+
+#Affect is a dict indexed by edges, and with affect's values
+affect = nx.get_edge_attributes(G, 'affect')
