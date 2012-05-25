842c5118d7dbb7e7e0a9ab93f76184e45cfd9024
bastien.gandouet@student.ecp.fr <bastien.gandouet@student.ecp.fr>
2012-05-25 16:09:22 +0200

    Initial graph generator

---
+++ graphgen.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+import networkx as nx
+
+def generator(n):
+	'''Generator for our social graph.
+
+	Our graph will have n nodes, it will be a Dorogovtsev-Mendes's Graph, and
+	it will handle reputation (for 	nodes) and affect (for edges).'''
+	
+	pass
