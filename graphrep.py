d1cd0fd4d97e5643d123110bc2472ba5b87cdf99
Yoann Delouis <ydelouis@gmail.com>
2012-05-25 17:03:19 +0200

    Add function updateReputation in graphrep.py

---
+++ graphrep.py
@@ -0,0 +1,16 @@
+import networkx as nx
+
+def updateReputation(G):
+    '''
+    Update the reputation of all nodes and return the new graph
+    The new reputation of node n is :
+         n.rep(t+1) = n.rep(t) + sum({(f.rep(t)-n.rep(t))/2*affect(n,f)/deg(n) , f friend of n})
+    '''
+    newRep = {}
+    affect = nx.get_edge_attributes(G, 'affect')
+    for node in G.nodes():
+        newRep[node] = G.node[node]['reput']
+        for friend in G.neighbors(node):
+            newRep[node] += (G.node[friend]['reput'] - G.node[node]['reput'])/2 * affect(node, friend)/G.degree(node)
+    nx.set_node_attributes(G, 'reput', newRep)
+    return G
