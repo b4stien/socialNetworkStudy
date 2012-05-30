46e0074adc06e283bb2dbcb3c1ebb5cafc595d53
bastien.gandouet@student.ecp.fr <bastien.gandouet@student.ecp.fr>
2012-05-30 13:22:12 +0200

    Working generator and some examples

--- graphgen.py
+++ graphgen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import networkx as nx
+import random
 
 def generator(n):
 	'''Generator for our social graph.
@@ -8,4 +9,43 @@ def generator(n):
 	Our graph will have n nodes, it will be a Dorogovtsev-Mendes's Graph, and
 	it will handle reputation (for 	nodes) and affect (for edges).'''
 	
-	pass
+	G = nx.dorogovtsev_goltsev_mendes_graph(1)
+	
+	'''In this loop we generate a raw network'''
+	for i in range(n-3):
+		r = random.choice(range(nx.number_of_edges(G)))
+		
+		e = nx.edges(G)
+		e = e[r] #e is the selected edge
+		
+		nn = G.number_of_nodes() #nn is the new node number
+
+		G.add_node(nn)
+
+		G.add_edge(nn, e[0])
+		G.add_edge(nn, e[1])
+
+	'''In this bloc we set reputations'''
+	reput = {}
+	md = max_degree(G)
+	for i in range(G.number_of_nodes()):
+		reput[i] = float(G.degree(i))/float(md)
+
+	nx.set_node_attributes(G,'reput',reput)
+
+	'''And then we set affect'''
+	affect = {}
+	e = nx.edges(G)
+	for i in range(G.number_of_edges()):
+		affect[e[i]] = random.random()
+
+	nx.set_edge_attributes(G,'affect',affect)	
+
+	return G
+
+def max_degree(G):
+	'''We search the maximum degree of G's nodes '''
+	m = 0
+	for i in range(G.number_of_nodes()):
+		if G.degree(i) >= m : m = G.degree(i)
+	return m
--- socialgraph.py
+++ socialgraph.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 
 
 #Generating a Graph with 5 nodes
-G = graphgen.generator(5)
+G = graphgen.generator(500)
 
 #Affect is a dict indexed by edges, and with affect's values
 affect = nx.get_edge_attributes(G, 'affect')
+
+print affect[(1,2)]
